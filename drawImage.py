9b4c838e5710fa65809d7312167d8ac6eaa6fdaa
hzxGoForward <hzx@pku.edu.cn>
2021-02-01 23:35:04 +0800

    生成80个数字

--- drawImage.py
+++ drawImage.py
@@ -47,9 +47,9 @@ def drawPic2(x_list, y_list, label_list):
 if __name__ == "__main__":
     kd = 0.00001
     m = 0.00001
-    x_list = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25,
-                       26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 60, 70, 80])
+    x_list = np.array([list(range(1, 81))])
+    print(x_list)
     y_list1 = np.array([func1(e, kd, m) for e in x_list])
     y_list2 = np.array([func2(e, kd, m) for e in x_list])
     y_list3 = np.array([func3(e, kd, m) for e in x_list])
-    drawPic2(x_list, np.array([y_list1, y_list2, y_list3]))
+    # drawPic2(x_list, np.array([y_list1, y_list2, y_list3]))
